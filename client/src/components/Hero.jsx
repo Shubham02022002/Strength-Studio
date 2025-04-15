659e8a4d33b83ac01dc9b430bad0248e9a696d70
Shubham <chaudhary2001shubham@gmail.com>
2025-04-15 18:05:58 +0530

    Some tweaks

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,8 +6,17 @@ function Hero() {
   return (
     <div className="hero-section">
       <div className="message">
-        <Typography variant="h5">Unlish the NEW YOU</Typography>
-        <Typography variant="h2">EASE WITH OUR GYM</Typography>
+      <Typography variant="h4">
+          Unleash the {" "}
+          <svg
+            className="neon-border"
+            viewBox="0 0 500 100"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <text x="40" y="130" className="neon-stroke">NEW YOU</text>
+          </svg>
+        </Typography>
+        <Typography variant="h2" fontFamily={'fantasy'} letterSpacing={3}>EASE WITH OUR GYM</Typography>
         <Link to='/plans' style={{
           textDecoration: 'none',
           scrollBehavior: 'smooth'
